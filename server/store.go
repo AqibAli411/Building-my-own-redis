189bba68ab48c6c45bfaddcfe7a262e1b82ca462
Aqib Ali <aqibhalepoto78@gmail.com>
2026-06-13 17:05:00 +0500

    Integrate custom HMap as primary server database

--- server/store.go
+++ server/store.go
@@ -2,16 +2,42 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
+	"aqib.builds/utils"
 )
 
 type ServerState struct {
-	kv map[string]string
+	DB utils.HMap
 }
 
 var Gdata ServerState
 
-func init() {
-	Gdata.kv = make(map[string]string)
+func NodeEq(a *utils.HNode, b *utils.HNode) bool {
+	za := (*utils.Entry)(unsafe.Pointer(a))
+	zb := (*utils.Entry)(unsafe.Pointer(b))
+	return za.Key == zb.Key
+}
+
+func lookUpOrCreate(key string) *utils.Entry {
+	entry := &utils.Entry{Key: key, Type: utils.T_STR, HeapIndex: -1}
+	entry.Node.Hcode = utils.HashKey(key)
+	from := utils.HmapLookup(&Gdata.DB, &entry.Node, NodeEq)
+	if from != nil {
+		return (*utils.Entry)(unsafe.Pointer(*from))
+	}
+	utils.HmapInsert(&Gdata.DB, &entry.Node)
+	return entry
+}
+
+func lookUpExisting(key string, expectedType utils.EntryType) (*utils.Entry, error) {
+	entry := &utils.Entry{Key: key, Type: expectedType, HeapIndex: -1}
+	entry.Node.Hcode = utils.HashKey(key)
+	node := utils.HmapLookup(&Gdata.DB, &entry.Node, NodeEq)
+	if node == nil {
+		return nil, nil
+	}
+	entry = (*utils.Entry)(unsafe.Pointer(*node))
+	return entry, nil
 }
 
 func DoRequest(cmd []string, out *[]byte) {
@@ -20,29 +46,24 @@ func DoRequest(cmd []string, out *[]byte) {
 	}
 	switch cmd[0] {
 	case "get":
-		if len(cmd) < 2 {
-			*out = []byte("err: invalid args")
-			return
-		}
-		val, ok := Gdata.kv[cmd[1]]
-		if !ok {
+		entry, _ := lookUpExisting(cmd[1], utils.T_STR)
+		if entry == nil {
 			*out = []byte("nil")
 		} else {
-			*out = []byte(val)
+			*out = []byte(entry.Str)
 		}
 	case "set":
-		if len(cmd) < 3 {
-			*out = []byte("err: invalid args")
-			return
-		}
-		Gdata.kv[cmd[1]] = cmd[2]
+		entry := lookUpOrCreate(cmd[1])
+		entry.Str = cmd[2]
 		*out = []byte("OK")
 	case "del":
-		if len(cmd) < 2 {
-			*out = []byte("err: invalid args")
-			return
+		entry := &utils.Entry{Key: cmd[1]}
+		entry.Node.Hcode = utils.HashKey(cmd[1])
+		from := utils.HmapDetach(&Gdata.DB, &entry.Node, NodeEq)
+		if from == nil {
+			*out = []byte("0")
+		} else {
+			*out = []byte("1")
 		}
-		delete(Gdata.kv, cmd[1])
-		*out = []byte("OK")
 	}
 }
